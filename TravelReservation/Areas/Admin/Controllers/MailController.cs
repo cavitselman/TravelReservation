596c893ae564bd7c8200e09bf6fec770b2aeaf61
Cavit Selman <tetikci462@hotmail.com>
2024-05-03 19:52:08 +0300

    Sending e-mail via SMTP.

---
+++ TravelReservation/Areas/Admin/Controllers/MailController.cs
@@ -0,0 +1,39 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using TravelReservation.Models;
+
+namespace TravelReservation.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(MailRequest mailRequest)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+
+            MailboxAddress mailboxAddress = new MailboxAddress("Admin", "deneme@gmail.com");
+
+            mimeMessage.From.Add(mailboxAddress);
+
+            MailboxAddress mailboxAdressTo = new MailboxAddress("User", mailRequest.ReceiverMail);
+            mimeMessage.To.Add(mailboxAdressTo);
+
+            mimeMessage.Subject = mailRequest.Subject;
+
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com", 587, false);
+            client.Authenticate("deneme@gmail.com", "deneme123");
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+            return View();
+        }
+    }
+}
